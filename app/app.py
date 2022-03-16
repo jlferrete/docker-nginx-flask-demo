9c7ec2245eb26f2e268bdff3a016f9318dfee465
Jlferrete <joseluisferrete@gmail.com>
2022-03-16 20:57:40 +0100

    Added posts

--- app/app.py
+++ app/app.py
@@ -6,6 +6,15 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,6 +23,11 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
     
 if __name__ == "__main__":
     app.run()
